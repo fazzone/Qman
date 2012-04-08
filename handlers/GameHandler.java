35f4fea03e555ff76c55cc0ebb87744b4cbbbe26
Russell McQueeney <justru@comcast.net>
2012-04-08 19:03:03 -0400

    added check for the necessary trailing slash in GameHandler.java

--- handlers/GameHandler.java
+++ handlers/GameHandler.java
@@ -12,6 +12,11 @@ import util.NoncePool;
 public class GameHandler implements PageHandler {
 	static NoncePool<Match> outstandingMatches = new NoncePool<Match>();
 	public void handleRequest(String args, PrintStream out) throws IOException {
+		//check if someone is just typing links and not putting the requisite '/'
+		if (!args.endsWith("/")) {
+			System.out.println("args = \""+args+"\"; no slash, aborting");
+			return;
+		}
 		String user = args.substring(0, args.length()-1);
 		if (!GameRuntime.users.containsKey(user))
 			GameRuntime.addUser(user);
